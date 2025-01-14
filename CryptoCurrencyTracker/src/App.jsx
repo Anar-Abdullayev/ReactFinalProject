b6edc631dc38e0e67636b20723187a3efdf3b0a7
Anar Abdullayev <anar.abdlyv@gmail.com>
2025-01-14 08:43:13 +0400

    ...

---
+++ CryptoCurrencyTracker/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/header/header'
+import CryptoTable from './components/table/table'
+
+function App() {
+  
+  return (
+    <>
+      <Header/>
+      <CryptoTable/>
+    </>
+  )
+}
+
+export default App
